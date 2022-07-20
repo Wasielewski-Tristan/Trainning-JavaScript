bf8e93d8a1f0ee4d46a7a92b3b2a90dd2482f586
TristanW <tristan.wasielewski@outlook.fr>
2022-07-20 16:33:06 +0200

    exo 1 JavaScript

---
+++ scriptexo1.js
@@ -0,0 +1,42 @@
+//1 console.log("Hello World");
+
+
+//2 console.log("Hello World");
+//  document.body.innerHTML = "Hello world";
+
+
+//3 var helloWorldH1 = document.createElement("h1");
+//     helloWorldH1.innerHTML = "hello world";
+//     document.body.appendChild(helloWorldH1);
+
+
+//4 var helloWorldH1 = document.createElement("h1");
+//     helloWorldH1.innerHTML = "hello world";
+//     document.querySelector("#greetings").appendChild(helloWorldH1);
+
+
+//5 var students = ["Alexandre","Béatrice","Benoît","Émeric","Florian","Gwenaëlle","Jérémy","Johan","Justine M","Justine P","Justine T","Kévin","Loïc","Mathieu","Matthias","Rémi","Samuel","Sébastien","Sullivan","Thomas","Tiffany","Valentin","Yann"];
+
+// var divGreet = document.querySelector("#studentsList");
+
+// students.forEach(function(student) {
+// divGreet.innerHTML += `<li>${student}</li>`
+// })
+
+
+
+// function loadStyle(event) {
+//     event.target.className = "dynamicStyle"
+// }
+
+// document.querySelector("main").addEventListener("click", loadStyle)
+
+
+// function calculator(cal1, cal2){
+//     cal1 = parseInt(document.querySelector("#cal1").value);
+//     cal2 = parseInt(document.querySelector("#cal2").value);
+//     const result = cal1+cal2;
+//     document.querySelector("#result").innerHTML = `Resultat: ${result}`;
+// }
+// document.querySelector("#calcul").addEventListener("click", calculator);
+
